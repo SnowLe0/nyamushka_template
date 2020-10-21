51ea52563da6bd10aea0d61f25d4884906bab915
SnowLeo <prigodaan@bk.ru>
2020-10-21 23:20:30 +0300

    Build

---
+++ precache-manifest.0115456cda12e05962b36c23d126e0c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d790f3daaa6bfcc84f82bb4312a3c7ec",
+    "url": "/nyamushka_template/index.html"
+  },
+  {
+    "revision": "f4acc040a17f406f508f",
+    "url": "/nyamushka_template/static/css/main.9f30dcc5.chunk.css"
+  },
+  {
+    "revision": "a0391d2227463c93d872",
+    "url": "/nyamushka_template/static/js/2.d9c074b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nyamushka_template/static/js/2.d9c074b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4acc040a17f406f508f",
+    "url": "/nyamushka_template/static/js/main.a3eead8f.chunk.js"
+  },
+  {
+    "revision": "50034f29a6677ed1e632",
+    "url": "/nyamushka_template/static/js/runtime-main.4266a07c.js"
+  },
+  {
+    "revision": "99e40e48b99de77cb4734604f1d362c1",
+    "url": "/nyamushka_template/static/media/Cat.99e40e48.png"
+  },
+  {
+    "revision": "21badd0cda99f7bd084aa49168e008c1",
+    "url": "/nyamushka_template/static/media/Exo2.0-Thin.21badd0c.woff"
+  },
+  {
+    "revision": "15ae772499285cd1361e0d4035138124",
+    "url": "/nyamushka_template/static/media/Trebuchet-MS.15ae7724.woff"
+  }
+]);
